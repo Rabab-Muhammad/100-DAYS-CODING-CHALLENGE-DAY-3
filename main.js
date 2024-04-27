988d18008e48048b204f554d7c08ebfbb510de3f
Rabab-Muhammad <rababmuhammadhussain@gmail.com>
2024-04-27 20:06:55 +0500

    push 100 days of coding challenge day 3 on github

---
+++ main.js
@@ -0,0 +1,19 @@
+//         100 DAYS OF CPDING CHALLENGE
+//             "Day 3 Challenge: Start Coding!"
+//Q1) Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//     Enclose your operations in print statements.
+console.log(4 + 4);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//Q2) You should create four lines that look like this:
+//   console.log(5 + 3)
+//   Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+//Q3)Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number.
+//    Print that message.
+var FavoriteNumber = 7;
+console.log("My favorite number is:", FavoriteNumber);
